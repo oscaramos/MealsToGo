820c4d98434b84d64097c46c436d49038db4dba5
Oscar <oscar.ramos@ucsp.edu.pe>
2021-03-16 21:10:51 -0500

    🚚 move: Extracted restaurants screen to their own file

--- App.tsx
+++ App.tsx
@@ -1,41 +1,13 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-import { Searchbar } from "react-native-paper";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { ThemeProvider } from "styled-components";
+
+import { RestaurantsScreen } from "./src/features/restaurant/screens/restaurants.screen";
+import { theme } from "./src/infrastructure/theme";
 
 export default function App() {
-  const [searchQuery, setSearchQuery] = React.useState("");
-
-  const onChangeSearch = (query: string) => setSearchQuery(query);
-
   return (
-    <SafeAreaView style={styles.container}>
-      <View style={styles.searchContainer}>
-        <Searchbar
-          placeholder="Search"
-          onChangeText={onChangeSearch}
-          value={searchQuery}
-        />
-      </View>
-      <View style={styles.listContainer}>
-        <Text>List</Text>
-      </View>
-    </SafeAreaView>
+    <ThemeProvider theme={theme}>
+      <RestaurantsScreen />
+    </ThemeProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: "column",
-  },
-  searchContainer: {
-    flexGrow: 0,
-    padding: 16,
-  },
-  listContainer: {
-    flexGrow: 1,
-    backgroundColor: "green",
-    padding: 16,
-  },
-});
---
+++ src/features/restaurant/screens/restaurants.screen.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import styled from "styled-components/native";
+import { Searchbar } from "react-native-paper";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+import { RestaurantInfoCard } from "../components/restaurant-info-card.component";
+
+const Container = styled(SafeAreaView)`
+  flex: 1;
+  flex-direction: column;
+`;
+
+const SearchContainer = styled.View`
+  flex-grow: 0;
+  padding: ${(props) => props.theme.space[3]};
+`;
+
+const ListContainer = styled.View`
+  flex-grow: 1;
+  background-color: green;
+  padding: ${(props) => props.theme.space[3]};
+`;
+
+export function RestaurantsScreen() {
+  const [searchQuery, setSearchQuery] = React.useState("");
+
+  const onChangeSearch = (query: string) => setSearchQuery(query);
+
+  return (
+    <Container>
+      <SearchContainer>
+        <Searchbar
+          placeholder="Search"
+          onChangeText={onChangeSearch}
+          value={searchQuery}
+        />
+      </SearchContainer>
+      <ListContainer>
+        <RestaurantInfoCard />
+      </ListContainer>
+    </Container>
+  );
+}
