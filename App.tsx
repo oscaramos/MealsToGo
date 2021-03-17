da3165c6c92ef8a9748b5bd5332dd167ce83d988
Oscar <oscar.ramos@ucsp.edu.pe>
2021-03-16 22:34:26 -0500

    💄 UI: Added fonts

--- App.tsx
+++ App.tsx
@@ -1,10 +1,23 @@
 import React from "react";
+import { AppLoading } from "expo";
 import { ThemeProvider } from "styled-components";
+
+import { Lato_400Regular } from "@expo-google-fonts/lato";
+import { useFonts, Oswald_400Regular } from "@expo-google-fonts/oswald";
 
 import { RestaurantsScreen } from "./src/features/restaurant/screens/restaurants.screen";
 import { theme } from "./src/infrastructure/theme";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Oswald_400Regular,
+    Lato_400Regular,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
   return (
     <ThemeProvider theme={theme}>
       <RestaurantsScreen />
--- src/features/restaurant/components/restaurant-info-card.component.tsx
+++ src/features/restaurant/components/restaurant-info-card.component.tsx
@@ -2,25 +2,47 @@ import React from "react";
 import { Card, Text } from "react-native-paper";
 import styled from "styled-components/native";
 
-const Title = styled(Text)`
+const RestaurantCard = styled(Card)`
+  background-color: ${(props) => props.theme.colors.bg.primary};
+`;
+
+const RestaurantCardCover = styled(Card.Cover)`
   padding: ${(props) => props.theme.space[3]};
+  background-color: ${(props) => props.theme.colors.bg.primary};
+`;
+
+const Info = styled(Card.Content)`
+  padding: ${(props) => props.theme.space[3]};
+`;
+
+const Title = styled(Text)`
   color: ${(props) => props.theme.colors.ui.primary};
+  font-family: ${(props) => props.theme.fonts.heading};
+  font-size: ${(props) => props.theme.fontSizes.title};
+`;
+
+const Address = styled(Text)`
+  color: ${(props) => props.theme.colors.ui.primary};
+  font-family: ${(props) => props.theme.fonts.body};
+  font-size: ${(props) => props.theme.fontSizes.caption};
 `;
 
 export function RestaurantInfoCard() {
   const title = "Some restaurant";
+  const address = "Avenida Benito 102";
 
   return (
-    <Card elevation={5}>
-      <Card.Cover
+    <RestaurantCard elevation={5}>
+      <RestaurantCardCover
         source={{
           uri:
             "https://www.foodiesfeed.com/wp-content/uploads/2019/06/top-view-for-box-of-2-burgers-home-made-600x899.jpg",
         }}
       />
-      <Card.Content>
+      <Info>
         <Title>{title}</Title>
-      </Card.Content>
-    </Card>
+        <Address>{address}</Address>
+      </Info>
+    </RestaurantCard>
   );
 }
