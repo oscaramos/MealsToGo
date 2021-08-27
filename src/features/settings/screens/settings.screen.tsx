fff6cd1cc3f766da67807a7712e160ad830148b9
oscaramos <odrr.99@gmail.com>
2021-08-26 23:00:08 -0500

    🚚 move(authentication): Moved logout to settings screen

--- src/features/settings/screens/settings.screen.tsx
+++ src/features/settings/screens/settings.screen.tsx
@@ -1,10 +1,16 @@
 import React from "react";
+import { Button } from "react-native";
 import { View, Text } from "react-native";
 
+import { useAuthentication } from "../../../services/authentication/authentication.context";
+
 export function SettingsScreen() {
+  const { logout } = useAuthentication();
+
   return (
     <View>
       <Text>SettingsScreen</Text>
+      <Button title="logout" onPress={() => logout()} />
     </View>
   );
 }
--- src/infrastructure/navigation/app.navigation.tsx
+++ src/infrastructure/navigation/app.navigation.tsx
@@ -1,16 +1,11 @@
-import React, { useEffect } from "react";
+import React from "react";
 
 import { AppNavigator } from "./app.navigator";
 import { AuthenticationNavigator } from "./authentication.navigator";
 import { useAuthentication } from "../../services/authentication/authentication.context";
 
 export const AppNavigation = () => {
-  const { user, login, logout } = useAuthentication();
-
-  useEffect(() => {
-    // login("test@test.com", "password123");
-    logout();
-  }, []);
+  const { user } = useAuthentication();
 
   return !user ? <AuthenticationNavigator /> : <AppNavigator />;
 };
