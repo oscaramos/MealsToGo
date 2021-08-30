bce5eb82c4c47aeb0ee69395d000aaf936bcd25d
oscaramos <odrr.99@gmail.com>
2021-08-29 23:38:10 -0500

    ✨ feat(camera): Added profile camera

--- src/features/settings/screens/camera.screen.tsx
+++ src/features/settings/screens/camera.screen.tsx
@@ -1,5 +1,36 @@
-import React from "react";
+import { Camera } from "expo-camera";
+import React, { useRef, useState, useEffect } from "react";
+import { View } from "react-native";
+import styled from "styled-components/native";
+
+import { Text } from "@components/typography/text.component";
+
+const ProfileCamera = styled(Camera)`
+  width: 100%;
+  height: 100%;
+`;
 
 export function CameraScreen() {
-  return null;
+  const [hasPermission, setHasPermission] = useState<null | boolean>(null);
+  const cameraRef = useRef<Camera | null>();
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestPermissionsAsync();
+      setHasPermission(status === "granted");
+    })();
+  }, []);
+
+  if (hasPermission === null) {
+    return <View />;
+  }
+  if (!hasPermission) {
+    return <Text>No access to camera</Text>;
+  }
+  return (
+    <ProfileCamera
+      ref={(camera) => (cameraRef.current = camera)}
+      type={Camera.Constants.Type.front}
+    />
+  );
 }
