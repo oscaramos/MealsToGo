1fd3c86484fc11f3176a161bc22652879ea559f3
oscaramos <odrr.99@gmail.com>
2021-11-01 23:26:46 -0500

    ✨ feat(functions-places): Integration with places photos API

--- functions/src/places/index.ts
+++ functions/src/places/index.ts
@@ -2,7 +2,20 @@ import { Client } from "@googlemaps/google-maps-services-js";
 import * as functions from "firebase-functions";
 import * as url from "url";
 
-import { addMockImage, mocks } from "./mock";
+import { BackendRestaurant } from "../../../types/restaurants";
+import { mocks } from "./mock";
+
+const addGoogleImage = (restaurant: BackendRestaurant) => {
+  const ref = restaurant.photos[0].photo_reference;
+  return {
+    ...restaurant,
+    photo: !ref
+      ? "https://www.foodiesfeed.com/wp-content/uploads/2019/06/top-view-for-box-of-2-burgers-home-made-600x899.jpg"
+      : `https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${ref}&key=${
+          functions.config().google.key
+        }`,
+  };
+};
 
 export async function placesRequest(
   request: functions.https.Request,
@@ -18,7 +31,7 @@ export async function placesRequest(
     // @ts-ignore
     const data = mocks[location];
     if (data) {
-      data.results = data.results.map(addMockImage);
+      data.results = data.results.map(addGoogleImage);
     }
     return response.json(data);
   }
@@ -34,7 +47,7 @@ export async function placesRequest(
       timeout: 1000,
     });
     // @ts-ignore
-    res.data.results = res.data.results.map(addMockImage);
+    res.data.results = res.data.results.map(addGoogleImage);
     return response.json(res.data);
   } catch (e) {
     response.status(400);
